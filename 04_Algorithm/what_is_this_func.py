ab2ffe073e831a12b99b48bdab065cc01d3e56f4
한림대학교 오픈소스 SW 교육센터 <38900144+Hallym-OpenSourceSW@users.noreply.github.com>
2019-05-14 01:01:16 +0900

    Create what_is_this_func.py

---
+++ 04_Algorithm/what_is_this_func.py
@@ -0,0 +1,10 @@
+"""
+다음 기술된 함수가 주어진 정수형 변수 XInput에 대해서 시행하는 기능은 무엇인가?
+"""
+
+def whatisThisFunction(XInput):
+    YOutput = 0
+    while XInput:
+        XInput &= (XInput-1)
+        YOutput += 1
+    return YOutput
