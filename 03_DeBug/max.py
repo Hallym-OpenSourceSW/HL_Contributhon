b2f2946d6f7c188f164baad60a52f2a9b00f7495
한림대학교 오픈소스 SW 교육센터 <38900144+Hallym-OpenSourceSW@users.noreply.github.com>
2019-05-13 14:48:59 +0900

    Create max.py

---
+++ 03_DeBug/max.py
@@ -0,0 +1,12 @@
+def find_max(nums):
+    max = nums[1]
+    for x in nums:
+      if x > max
+        max = x
+    print(max)
+
+def main():
+  find_max([2, 4, 9, 7, 19, 94, 5])
+
+if __name__ == '__main__':
+  main()
