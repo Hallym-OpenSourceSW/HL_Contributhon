df4e2b6b12d554153eee72fd57683c5229a7409e
한림대학교 오픈소스 SW 교육센터 <38900144+Hallym-OpenSourceSW@users.noreply.github.com>
2019-05-13 14:50:51 +0900

    Create absolute.py

---
+++ 03_DeBug/absolute.py
@@ -0,0 +1,14 @@
+def absVal(num)
+    """
+    Function to fins absolute value of numbers.
+    >>absVal(-5)
+    5
+    >>absVal(0)
+    0
+    """
+    if Num < 0:
+        return -num
+    else:
+        return num
+
+print(absVal(-34)) # = 34
