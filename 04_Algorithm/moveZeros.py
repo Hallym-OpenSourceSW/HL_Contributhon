3883e5df9a8d7597e71159e603c628ce54bbbb95
한림대학교 오픈소스 SW 교육센터 <38900144+Hallym-OpenSourceSW@users.noreply.github.com>
2019-05-14 00:50:02 +0900

    Create moveZeros.py

---
+++ 04_Algorithm/moveZeros.py
@@ -0,0 +1,20 @@
+"""
+배열을 입력 받아서 배열내에 있는 "0" 들을 모두 뒷부분으로 옮기는 알고리즘을 만들어 보세요!
+단, 배열내에 "0"이 아닌 요소들의 순서는 지켜져야 합니다.
+
+    moveZeros(["false", 1, 0, 1, 2, 0, 1, 3, "a"])
+    returns => ["false", 1, 1, 2, 1, 3, "a", 0, 0]
+    
+"""
+
+
+def moveZeros(array):
+    result = []
+    zeros = 0
+
+    # your code
+    # your code
+    
+    return result
+    
+print(moveZeros(["false", 1, 0, 1, 2, 0, 1, 3, "a"]))  ## ['false', 1, 1, 2, 1, 3, 'a', 0, 0]
