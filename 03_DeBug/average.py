4d59dcac73fded0a58987a283f862b9c05e0fbb6
한림대학교 오픈소스 SW 교육센터 <38900144+Hallym-OpenSourceSW@users.noreply.github.com>
2019-05-13 15:00:58 +0900

    Create average.py

---
+++ 03_DeBug/average.py
@@ -0,0 +1,11 @@
+def average(nums):
+    sum = 0
+    n = 0
+    for n in nums:
+      sum += x
+      n += 1
+    avg = sum / n
+    print(avg)
+
+# 평균값은 22.857142857142858 이여야 함.
+average([2, 4, 6, 8, 20, 50, 70]) 
