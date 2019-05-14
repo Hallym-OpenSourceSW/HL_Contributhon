ca7e31d04c321ce091376e1592db2e7666551db2
RMXth <40137813+RMXth@users.noreply.github.com>
2019-05-14 15:21:57 +0900

    Update sort.py
    merge sort를 이용하여 정렬함.

--- 04_Algorithm/sort.py
+++ 04_Algorithm/sort.py
@@ -4,12 +4,44 @@
 import timeit
 
 
-def sort(collection):
-  """최고의 성능을 가진 정렬 알고리즘을 만들어 보세요 ! """
-  # Your Code
-  # Your Code
-  return collection
+def sort(arr):
+    """ Merge Sort
+        Complexity: O(n log(n))
+    """
+    # Our recursive base case
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    # Perform merge_sort recursively on both halves
+    left, right = sort(arr[:mid]), sort(arr[mid:])
 
+    # Merge each side together
+    return merge(left, right, arr.copy())
+
+
+def merge(left, right, merged):
+    """ Merge helper
+        Complexity: O(n)
+    """
+
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+        # Sort each one and place into the result
+        if left[left_cursor] >= right[right_cursor]:
+            merged[left_cursor+right_cursor]=left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+    # Add the left overs if there's any left to the result
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+    # Add the left overs if there's any left to the result
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    # Return result
+    return merged
 
 # 메인 함수
 collection = [100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
@@ -22,4 +54,4 @@ print(collection)
 
 
 # 나의 수행 시간
-# 0.001913671999318467
+# 0.0003475579997029854
