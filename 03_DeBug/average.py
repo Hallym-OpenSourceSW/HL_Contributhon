cbe09ca6b182213a3fca4df17265b29dfac2456a
ihg101 <50610324+ihg101@users.noreply.github.com>
2019-05-14 15:04:38 +0900

    Update average.py

--- 03_DeBug/average.py
+++ 03_DeBug/average.py
@@ -9,3 +9,17 @@ def average(nums):
 
 # 평균값은 22.857142857142858 이여야 함.
 average([2, 4, 6, 8, 20, 50, 70]) 
+
+
+#수정
+def average(nums):
+    sum = 0
+    n = 0
+    for n in nums:
+      sum += n
+      
+    avg = sum / len(nums)
+    print(avg)
+
+# 평균값은 22.857142857142858 이여야 함.
+average([2, 4, 6, 8, 20, 50, 70])
