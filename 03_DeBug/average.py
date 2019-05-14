eac79c557dd0ffcedd8de92d45fdda67915db5ef
Shifty <note3352@gmail.com>
2019-05-14 14:48:27 +0900

    Update average.py
    sum += x --> sum += n
    cnt = 0 새로 만들고,
    n += 1 --> cnt += 1
    avg = sum / n --> avg = sum / cnt

--- 03_DeBug/average.py
+++ 03_DeBug/average.py
@@ -9,3 +9,16 @@ def average(nums):
 
 # 평균값은 22.857142857142858 이여야 함.
 average([2, 4, 6, 8, 20, 50, 70]) 
+
+def average(nums):
+    sum = 0
+    n = 0
+    cnt = 0
+    for n in nums:
+      sum += n
+      cnt += 1
+    avg = sum / cnt
+    print(avg)
+
+# 평균값은 22.857142857142858 이여야 함.
+average([2, 4, 6, 8, 20, 50, 70]) 
