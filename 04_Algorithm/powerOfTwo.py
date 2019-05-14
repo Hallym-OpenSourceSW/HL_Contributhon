33127b087f4c6b6d3ae2d23e2c28a9ab4b627abc
BJ-Lim <43159254+BJ-Lim@users.noreply.github.com>
2019-05-14 14:44:43 +0900

    developed powerOfTwo.py
    check_test() function is for test.

--- 04_Algorithm/powerOfTwo.py
+++ 04_Algorithm/powerOfTwo.py
@@ -4,3 +4,19 @@
 첫번째 기여자: 10점
 성능 향상 기여자: 5점
 """
+
+def check_bin(i):
+  if i <= 0:
+    return False
+  while i > 2:
+    r = i % 2
+    if r == 1:
+      return False
+    i = i / 2
+  return True
+
+def check_test():
+  for i in range(0,100):
+    print(i, check_bin(i)) 
+
+check_test()
