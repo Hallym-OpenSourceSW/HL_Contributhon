fad0c0e87c6c3dc76e1c333363279cf8a89597bc
vl0011 <48581180+vl0011@users.noreply.github.com>
2019-05-14 15:05:44 +0900

    Update absolute.py
    : 누락
    Num 오타

--- 03_DeBug/absolute.py
+++ 03_DeBug/absolute.py
@@ -1,6 +1,6 @@
 # 절대값을 찾는 함수 구현 및 테스트입니다. 어디가 잘못된 걸까요 ?
 
-def absVal(num)
+def absVal(num):
     """
     Function to fins absolute value of numbers.
     >>absVal(-5)
@@ -8,7 +8,7 @@ def absVal(num)
     >>absVal(0)
     0
     """
-    if Num < 0:
+    if num < 0:
         return -num
     else:
         return num
