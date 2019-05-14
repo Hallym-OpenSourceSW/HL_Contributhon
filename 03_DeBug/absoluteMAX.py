33190b1316562cc669c0d3280c0f0760da15c79a
ihg101 <50610324+ihg101@users.noreply.github.com>
2019-05-14 15:10:23 +0900

    Update absoluteMAX.py
    abs(i)  < abs(j)    ->  abs(i) > abs(j) 로 수정

--- 03_DeBug/absoluteMAX.py
+++ 03_DeBug/absoluteMAX.py
@@ -17,3 +17,23 @@ def absMax(x):
 
 a = [1,2,-11]
 print(absMax(a)) # = -11
+
+
+#수정
+
+def absMax(x):
+    """
+    #>>>absMax([0,5,1,11])
+    11
+    >>absMax([3,-10,-2])
+    -10
+    """
+    j =x[0]
+    for i in x:
+        if abs(i) > abs(j):
+            j = i
+    return j
+
+
+a = [1,2,-11]
+print(absMax(a)) # = -11
