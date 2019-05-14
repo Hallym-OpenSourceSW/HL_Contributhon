ad4f7e27c41f7acc6a87dcd17f267150f21944a9
ihg101 <50610324+ihg101@users.noreply.github.com>
2019-05-14 15:05:40 +0900

    Update sum_to_n.py
    반복문에서 맨 마지막숫자를 안더해서  a -> a+1로 수정

--- 03_DeBug/sum_to_n.py
+++ 03_DeBug/sum_to_n.py
@@ -10,3 +10,17 @@ def sum_a(a):
 # 예제 실습
 print(sum_a(10)) # 1부터 10까지의 합 55이 나오면 성공
 print(sum_a(100)) # 1부터 100까지의 합 5050이 나오면 성공
+
+
+#수정
+
+def sum_a(a):
+    b = 0
+    for i in range(1,a+1):
+        b+=i
+    return b
+
+
+# 예제 실습
+print(sum_a(10)) # 1부터 10까지의 합 55이 나오면 성공
+print(sum_a(100)) # 1부터 100까지의 합 5050이 나오면 성공
