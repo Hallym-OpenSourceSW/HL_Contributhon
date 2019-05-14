7bb12bb3afe5762a2b136f9a30a71d1c55e16e41
JeongMyeong <44393016+JeongMyeong@users.noreply.github.com>
2019-05-14 15:17:59 +0900

    Update [average.py] JM
    Update [average.py] JM
    수정하여 완성하였습니다.
    정의 되지 않은 x를 원하는 알고리즘에 맞게 수정하였고
    for 의 변수로 사용되는 n이 있어 수정하였습니다.

--- 03_DeBug/average.py
+++ 03_DeBug/average.py
@@ -1,10 +1,8 @@
 def average(nums):
     sum = 0
-    n = 0
     for n in nums:
-      sum += x
-      n += 1
-    avg = sum / n
+        sum += n
+    avg = sum / len(nums)
     print(avg)
 
 # 평균값은 22.857142857142858 이여야 함.
